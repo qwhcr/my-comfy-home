e1c4084d425940f74ac62644986587de605196b4
Will Huang <qhuang79@gatech.edu>
2021-01-01 01:58:19 +0000

    adding ditribute script, updating vimrc on coc

---
+++ distribute.sh
@@ -0,0 +1,4 @@
+rm ~/.tmux.conf
+cp ./configs/tmux/tmux.conf ~/.tmux.conf
+rm ~/.vimrc
+cp ./configs/vim/vimrc ~/.vimrc
--- update-tmux-vim.sh
+++ update-tmux-vim.sh
@@ -1,14 +1,13 @@
 #clean up previous state
-rm -rf ./configs || true
+rm -rf ./configs/vim || true
+rm -rf ./configs/tmux || true
 
 #directory setup
-mkdir ./configs
+mkdir ./configs 2>/dev/null || true
 
 #copy most updated configs
 mkdir ./configs/vim
 cp ~/.vimrc ./configs/vim/vimrc
 mkdir ./configs/tmux
 cp ~/.tmux.conf ./configs/tmux/tmux.conf
-mkdir ./configs/zsh
-cp ~/.zshrc ./configs/zsh/zshrc
 echo 'update complete'
